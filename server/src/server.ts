b99d8adc439e24c3c55a93fa0184b343a485ae33
divlo <contact@divlo.fr>
2020-11-21 17:21:42 +0100

    chore: delete Microsoft LICENSE in src files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 import * as async from 'async'
 import * as deglob from 'deglob'
 import * as fs from 'fs'
