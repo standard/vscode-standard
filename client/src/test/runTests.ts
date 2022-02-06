20f5bc0dad0ffd13ab3355698c9af6d5af6c3b21
Divlo <contact@divlo.fr>
2022-02-06 11:34:52 +0100

    test: add `standard-legacy`

--- client/src/test/runTests.ts
+++ client/src/test/runTests.ts
@@ -1,12 +1,48 @@
 import * as path from 'path'
+import * as fs from 'fs'
+import { exec } from 'child_process'
 import { runTests } from '@vscode/test-electron'
-import { linterValues } from '../linterValues'
+
+/**
+ * Executes a shell command and return it as a Promise.
+ * @param cmd {string}
+ * @return {Promise<string>}
+ */
+async function execShellCommand (cmd: string): Promise<string> {
+  return await new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error != null) {
+        console.warn(error)
+        reject(error)
+      } else {
+        resolve(stdout.length > 0 ? stdout : stderr)
+      }
+    })
+  })
+}
+
+async function getDirectories (source: string): Promise<string[]> {
+  return (await fs.promises.readdir(source, { withFileTypes: true }))
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+}
 
 async function main (): Promise<void> {
-  for (const linter of linterValues) {
-    const extensionDevelopmentPath = path.resolve(__dirname, '../../../')
+  const testFixtures = await getDirectories(
+    path.join(__dirname, '..', '..', 'testFixture')
+  )
+  for (const linter of testFixtures) {
+    const extensionDevelopmentPath = path.resolve(__dirname, '..', '..', '..')
     const extensionTestsPath = path.resolve(__dirname, linter, 'index')
-    const testWorkspace = path.resolve(__dirname, '../../testFixture', linter)
+    const testWorkspace = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'testFixture',
+      linter
+    )
+    process.chdir(testWorkspace)
+    await execShellCommand('npm install')
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
@@ -15,7 +51,7 @@ async function main (): Promise<void> {
   }
 }
 
-main().catch(err => {
+main().catch((err) => {
   console.error(err)
   console.error('Failed to run tests')
   process.exit(1)
